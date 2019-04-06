d3fadbb23b5ef917bd90350512f5323ee15b99a1
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-04-06 16:23:36 +0200

    Interact mechanic start

---
+++ tycoon/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+	public GameObject ui;
+	public string text;
+	private Text interactText;
+
+	private void Awake () {
+		interactText = GameObject.FindWithTag("InteractText").GetComponent<Text>();
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if(other.tag == "Player")
+		{
+			interactText.text = text;
+		}
+	}
+
+	private void OnTriggerExit (Collider other) {
+		if(other.tag == "Player")
+		{
+			interactText.text = null;
+		}
+	}
+}
