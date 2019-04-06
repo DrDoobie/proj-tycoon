c198034bb68e5921ce8c979030ccbb4de65d0b89
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-04-06 15:58:58 +0200

    Open and close ui, pause

--- tycoon/Assets/GameController.cs
+++ tycoon/Assets/GameController.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool uiOpen, paused;
+	public GameObject uiPanel;
+
+	private void Update () {
+		PauseController();
+		UIController();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void PauseController () {
+		if(Input.GetButtonDown("Pause"))
+		{
+			paused = !paused;
+		}
+
+		if(paused)
+		{
+			CursorLock(false);
+			Time.timeScale = 0.0f;
+			return;
+		}
+
+		CursorLock(true);
+		Time.timeScale = 1.0f;
+	}
+
+	private void UIController () {
+		if(Input.GetButtonDown("UI"))
+		{
+			uiOpen = !uiOpen;
+		}
+
+		if(uiOpen)
+		{
+			uiPanel.SetActive(true);
+			return;
+		}
+
+		uiPanel.SetActive(false);
+	}
+
+	public void CursorLock (bool bol) {
+		if(bol)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+			return;
+		}
+
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
 	}
 }
