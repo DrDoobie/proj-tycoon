034a36343c2de2f9a315d35cc7a3d4783b6c0137
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-04-13 10:45:16 +0200

    You can now place plant pots

---
+++ tycoon/Assets/Scripts/PlacementPrefab.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementPrefab : MonoBehaviour {
+
+    public bool placeable;
+    public GameObject objPrefab;
+    public Material[] material;
+    Renderer rend;
+
+	private void Awake () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    private void Update () {
+        PlacementController();
+    }
+
+    private void PlacementController () {
+        if(!placeable)
+        {
+            rend.sharedMaterial = material[1];
+            return;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(objPrefab, transform.parent.position, transform.parent.rotation);
+        }
+
+        rend.sharedMaterial = material[0];
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        if(other.gameObject.layer == 9)
+        {
+            placeable = false;
+            Debug.Log("AWD");
+        }
+    }
+}
