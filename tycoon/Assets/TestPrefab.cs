972e2da7561ac1b82fcff71ed5e5e5a65b5e5af2
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-05-21 11:05:06 +0200

    info panel for pot objects

---
+++ tycoon/Assets/TestPrefab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPrefab : MonoBehaviour {
+
+	public bool testBool;
+	public GameObject infoPanel;
+
+	private void Update () {
+        NewMethod();
+    }
+
+    private void NewMethod () {
+        if(testBool)
+        {
+            infoPanel.SetActive(true);
+            return;
+        }
+
+        infoPanel.SetActive(false);
+    }
+}
